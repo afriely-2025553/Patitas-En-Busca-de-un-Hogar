debc415a438c2936b26a534aa1101723e2988e73
Andres Aquino <aaquino-2023534@kinal.edu.gt>
2026-07-22 11:10:40 -0600

    feat: se modifico la base de datos

--- patitasEnBuscaDeHogar-DB.sql
+++ patitasEnBuscaDeHogar-DB.sql
@@ -3,20 +3,36 @@ create database PatitasEnBuscaDeHoga_in5cm;
 
 use PatitasEnBuscaDeHoga_in5cm;
 
-create table Refugio(
+create table Especies(
+	id_especie int auto_increment,
+	nombreEspecie varchar(100),
+    primary key PK_id_especie(id_especie)
+);
+
+create table Veterinarios(
+	id_veterinario int auto_increment,
+	nombreVeterinario varchar(100),
+	telefonoVeterinario bigint,
+    primary key PK_id_veterinario(id_veterinario)
+);
+
+create table Refugios(
 	id_refugio int auto_increment,
 	nombreRefugio varchar(100),
 	direccionRefugio varchar(200),
     telefonoRefugio bigint,
     primary key PK_id_refugio(id_refugio)
 );
-create table Especie(
-	id_especie int auto_increment,
-	nombreEspecie varchar(100),}
-    primary key PK_id_especie(id_especie)
+
+create table Adoptantes(
+	id_adoptante int auto_increment,
+	nombreAdoptante varchar(100),
+	telefonoAdoptante bigint,
+	direccion varchar(200),
+    primary key id_adoptante(id_adoptante)
 );
 
-create table Raza(
+create table Razas(
 	id_raza int auto_increment,
 	nombreRaza varchar(100),
 	id_especie int,
@@ -25,7 +41,52 @@ create table Raza(
 	references Especie(id_especie) on delete cascade
 );
 
-create table Animal(
+create table Donaciones(
+	id_donacion int auto_increment,
+    tipoDonacion enum ('Monetaria', 'Bienes Materiales'),
+    montoDonacion decimal (10,2),
+    fechaDonacion date,
+    id_refugio int,
+    primary key id_donacion(id_donacion),
+    constraint Fk_id_refugio foreign key (id__refugio)
+    references Refugios(id_refugio) on delete cascade
+);
+
+create table Voluntaios(
+	id_voluntario int auto_increment,
+    nombreVoluntario varchar (100),
+    telefonoVoluntario varchar(10),
+    id_refugio int,
+    primary key id_voluntario(id_voluntario),
+    constraint Fk_id_refugio foreign key (id__refugio)
+    references Refugios(id_refugio) on delete cascade
+);
+
+create table Vacunas(
+	id_vacuna int auto_increment,
+	nombreVacuna enum('Antirrabica', 'Moquillo', 'Parvovirus', 'Complejos Respiratorios'),
+	fechaDeAplicacion date,
+    id_animal int,
+	id_veterinario int,
+    primary key id_vacuna(id_vacuna),
+    constraint Fk_id_animal foreign key (id_animal)
+    references Animal(id_animal) on delete cascade
+);
+
+create table Adopciones(
+	id_adopcion int auto_increment,
+	fechaDeAdopcion date,
+	id_animal int,
+	id_adoptante int,
+    primary key id_adopcion(id_adopcion),
+    constraint Fk_id_animal foreign key (id_animal)
+    references Animal(id_animal) on delete cascade,
+    constraint Fk_id_adoptante foreign key (id_adoptante)
+    references Adoptante(id_adoptante)
+);
+
+
+create table Animales(
 	id_animal int auto_increment,
 	nombreAnimal varchar(100),
 	edadAnimal int,
@@ -43,39 +104,11 @@ create table Animal(
     references Raza(id_raza)on delete cascade
 );
 
-create table Veterinario(
-	id_veterinario int auto_increment,
-	nombreVeterinario varchar(100),
-	telefonoVeterinario bigint,
-    primary key PK_id_veterinario(id_veterinario)
-);
-create table Vacuna(
-	id_vacuna int auto_increment,
-	nombreVacuna enum('Antirrabica', 'Moquillo', 'Parvovirus', 'Complejos Respiratorios').
-	fechaDeAplicacion date,
-    id_animal int,
-	id_veterinario int,
-    primary key id_vacuna(id_vacuna),
-    constraint Fk_id_animal foreign key (id_animal)
-    references Animal(id_animal) on delete cascade
-);
 
-create table Adoptante(
-	id_adoptante int auto_increment,
-	nombreAdoptante varchar(100),
-	telefonoAdoptante bigint,
-	direccion varchar(200),
-    primary key id_adoptante(id_adoptante)
-);
-create table Adopcion(
-	id_adopcion int auto_increment,
-	fechaDeAdopcion date,
-	id_animal int,
-	id_adoptante int,
-    primary key id_adopcion(id_adopcion),
-    constraint Fk_id_animal foreign key (id_animal)
-    references Animal(id_animal) on delete cascade,
-    constraint Fk_id_adoptante foreign key (id_adoptante)
-    references Adoptante(id_adoptante)
-);
+-- CRUD ESPECIES --
 
+-- CRUD VETERINARIOS --
+
+-- CRUD REFUGIOS --
+
+-- CRUD ADOPTANTES --
